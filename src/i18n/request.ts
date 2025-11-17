13f1e0e5ab06ab0ebc461858bfadfb3d64f69c12
Win1 <vuhuynh@var-meta.com>
2025-11-18 00:26:55 +0700

    fix: rollback rq config

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,24 +1,17 @@
 import {getRequestConfig} from 'next-intl/server';
 import {routing} from './routing';
+import { hasLocale } from 'next-intl';
  
-// export default getRequestConfig(async ({requestLocale}) => {
-//   const requested = await requestLocale;
-//   const locale = hasLocale(routing.locales, requested)
-//     ? requested
-//     : routing.defaultLocale;
-//     const messages = (await import(`../../locales/${locale}/home.json`)).default;
-
-//   return {
-//     locale,
-//     messages
-//   };
-// });
-
-export default getRequestConfig(async () => {
-    const locale = routing.defaultLocale // force default locale
+export default getRequestConfig(async ({requestLocale}) => {
+  const requested = await requestLocale;
+  const locale = hasLocale(routing.locales, requested)
+    ? requested
+    : routing.defaultLocale;
     const messages = (await import(`../../locales/${locale}/home.json`)).default;
+
   return {
     locale,
     messages
   };
 });
+
