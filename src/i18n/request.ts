2f67fd5f8227ad803157afa3a259779cd6366037
Win1 <vuhuynh@var-meta.com>
2025-11-18 00:16:04 +0700

    fix: fource locale

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,14 +1,22 @@
 import {getRequestConfig} from 'next-intl/server';
-import {hasLocale} from 'next-intl';
 import {routing} from './routing';
  
-export default getRequestConfig(async ({requestLocale}) => {
-  const requested = await requestLocale;
-  const locale = hasLocale(routing.locales, requested)
-    ? requested
-    : routing.defaultLocale;
-    const messages = (await import(`../../locales/${locale}/home.json`)).default;
+// export default getRequestConfig(async ({requestLocale}) => {
+//   const requested = await requestLocale;
+//   const locale = hasLocale(routing.locales, requested)
+//     ? requested
+//     : routing.defaultLocale;
+//     const messages = (await import(`../../locales/${locale}/home.json`)).default;
 
+//   return {
+//     locale,
+//     messages
+//   };
+// });
+
+export default getRequestConfig(async () => {
+    const locale = routing.defaultLocale // force default locale
+    const messages = (await import(`../../locales/${locale}/home.json`)).default;
   return {
     locale,
     messages
